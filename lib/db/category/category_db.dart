4d6b7e392617146cc187ff9e60074ecf5684d9a0
Akhil Valsamrithan <avalsamrithan@rbbn.com>
2022-01-11 19:02:41 +0530

    Add/Delete categories implemented

--- lib/db/category/category_db.dart
+++ lib/db/category/category_db.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:money_manager/models/category/category_model.dart';
 
@@ -7,18 +8,57 @@ const CATEGORY_DB_NAME = 'category_database';
 abstract class CategoryDbFunctions {
   Future<List<CategoryModel>> getCategories();
   Future<void> insertCategory(CategoryModel value);
+  Future<void> deleteCategory(String categoryID);
 }
 
 class CategoryDB implements CategoryDbFunctions {
+  //For making the class singleton
+  CategoryDB._internal();
+  static CategoryDB instance = CategoryDB._internal();
+  factory CategoryDB() {
+    return instance;
+  }
+  ValueNotifier<List<CategoryModel>> incomeCategoryListNotifier =
+      ValueNotifier([]);
+  ValueNotifier<List<CategoryModel>> expenseCategoryListNotifier =
+      ValueNotifier([]);
+
   @override
   Future<void> insertCategory(CategoryModel value) async {
     final _categoryDB = await Hive.openBox<CategoryModel>(CATEGORY_DB_NAME);
-    await _categoryDB.add(value);
+    //add object with id specified
+    await _categoryDB.put(value.id, value);
+    refreshUI();
   }
 
   @override
   Future<List<CategoryModel>> getCategories() async {
     final _categoryDB = await Hive.openBox<CategoryModel>(CATEGORY_DB_NAME);
     return _categoryDB.values.toList();
+  }
+
+  Future<void> refreshUI() async {
+    final _categoryDB = await getCategories();
+
+    //Clear current lists
+    incomeCategoryListNotifier.value.clear();
+    expenseCategoryListNotifier.value.clear();
+
+    await Future.forEach<CategoryModel>(_categoryDB, (category) {
+      if (category.type == CategoryType.income) {
+        incomeCategoryListNotifier.value.add(category);
+        incomeCategoryListNotifier.notifyListeners();
+      } else {
+        expenseCategoryListNotifier.value.add(category);
+        expenseCategoryListNotifier.notifyListeners();
+      }
+    });
+  }
+
+  @override
+  Future<void> deleteCategory(String categoryID) async {
+    final _categoryDB = await Hive.openBox<CategoryModel>(CATEGORY_DB_NAME);
+    _categoryDB.delete(categoryID);
+    refreshUI();
   }
 }
--- lib/screens/categories/category_add_popup.dart
+++ lib/screens/categories/category_add_popup.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:money_manager/db/category/category_db.dart';
 import 'package:money_manager/models/category/category_model.dart';
 
 ValueNotifier<CategoryType> _currentSelectionNotifier =
     ValueNotifier(CategoryType.income);
 
 Future<void> showCategoryAddPopup(BuildContext context) async {
+  final _categoryNameController = TextEditingController();
   showDialog(
     context: context,
     builder: (BuildContext ctx) {
@@ -15,6 +17,7 @@ Future<void> showCategoryAddPopup(BuildContext context) async {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextFormField(
+              controller: _categoryNameController,
               decoration: const InputDecoration(
                   hintText: 'Category',
                   border: OutlineInputBorder(
@@ -31,7 +34,25 @@ Future<void> showCategoryAddPopup(BuildContext context) async {
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(onPressed: () {}, child: const Text('Save')),
+            child: ElevatedButton(
+              onPressed: () {
+                final _name = _categoryNameController.text;
+                final _type = _currentSelectionNotifier.value;
+                if (_name.isEmpty) {
+                  return;
+                }
+
+                final _category = CategoryModel(
+                  id: DateTime.now().microsecondsSinceEpoch.toString(),
+                  name: _name,
+                  type: _type,
+                );
+                //Add the object to DB
+                CategoryDB.instance.insertCategory(_category);
+                Navigator.of(ctx).pop();
+              },
+              child: const Text('Save'),
+            ),
           ),
         ],
       );
